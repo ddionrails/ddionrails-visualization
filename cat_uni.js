1d8e6c0c0215b977cb1b004eb7a84e4316ae9a2c
Carolin Stolpe <cstolpe@diw.de>
2016-03-15 14:11:36 +0100

    Restructure code, add axis label to density chart

--- cat_uni.js
+++ cat_uni.js
@@ -1,68 +1,68 @@
  function cat_uni(options){
 
-	d3.selectAll(".chart").remove();
+	d3.selectAll('.chart').remove();
 			
-			//var rawData = d3.json("./testdata/edu.json");
+			//var rawData = d3.json('./testdata/edu.json');
 
 			var rawData = { 
 		  
-				"study":"soep-test",
-				"dataset":"test1",
-				"variable":"edu",
-				"label":"Hoechster Bildungsabschluss",
-				"uni":{
-					"frequencies":[20, 12,123,321,214,100],
-					"weighted":	  [30, 5, 123, 400, 200, 90],
-					"values":	  [-8,-2,-1,1,2,3,4],
-					"missings":[true, true,false,false,false,false],
-					"labels":["-8", "no response","Hauptschule","Realschule","Gymnasium","University"],
+				'study':'soep-test',
+				'dataset':'test1',
+				'variable':'edu',
+				'label':'Hoechster Bildungsabschluss',
+				'uni':{
+					'frequencies':[20, 12,123,321,214,100],
+					'weighted':	  [30, 5, 123, 400, 200, 90],
+					'values':	  [-8,-2,-1,1,2,3,4],
+					'missings':[true, true,false,false,false,false],
+					'labels':['-8', 'no response','Hauptschule','Realschule','Gymnasium','University'],
 				},
-				"bi":{
-					"sex":{
-						"label":"Geschlecht",
-						"categories":{
-							"0":{
-								"label":"Mann",
-								"frequencies":[4, 1, 2, 3, 4, 5],
-								"weighted":	  [5, 13, 3, 7, 9, 6],
+				'bi':{
+					'sex':{
+						'label':'Geschlecht',
+						'categories':{
+							'0':{
+								'label':'Mann',
+								'frequencies':[4, 1, 2, 3, 4, 5],
+								'weighted':	  [5, 13, 3, 7, 9, 6],
 							},
-							"1":{
-								"label":"Frau",
-								"frequencies":[4, 2, 3, 6, 8, 10],
-								"weighted":	  [2, 4, 3, 7, 11, 13],
+							'1':{
+								'label':'Frau',
+								'frequencies':[4, 2, 3, 6, 8, 10],
+								'weighted':	  [2, 4, 3, 7, 11, 13],
 							}
 						},
-						"values":[-8,-2,-1,1,2,3,4],
-						"missings":[true, true,false,false,false,false],
-						"labels":["-8", "no response","Hauptschule","Realschule","Gymnasium","University"],
+						'values':[-8,-2,-1,1,2,3,4],
+						'missings':[true, true,false,false,false,false],
+						'labels':['-8', 'no response','Hauptschule','Realschule','Gymnasium','University'],
 					},
-					"wave":{
-						"label":"Welle",
-						"categories":{
-							"0":{
-								"label":"94",
-								"frequencies":[30, 60, 10, 4, 24, 35],
-								"weighted":	  [20, 90, 10, 3, 20, 34],
+					'wave':{
+						'label':'Welle',
+						'categories':{
+							'0':{
+								'label':'94',
+								'frequencies':[30, 60, 10, 4, 24, 35],
+								'weighted':	  [20, 90, 10, 3, 20, 34],
 							},
-							"1":{
-								"label":"95",
-								"frequencies":[34, 33, 2, 9, 8, 10],
-								"weighted":	  [30, 30, 4, 10, 7, 6],
+							'1':{
+								'label':'95',
+								'frequencies':[34, 33, 2, 9, 8, 10],
+								'weighted':	  [30, 30, 4, 10, 7, 6],
 							},
-							"2":{
-								"label":"96",
-								"frequencies":[45, 70, 30, 65, 83, 10],
-								"weighted":	  [45, 80, 20, 50, 60, 6],
+							'2':{
+								'label':'96',
+								'frequencies':[45, 70, 30, 65, 83, 10],
+								'weighted':	  [45, 80, 20, 50, 60, 6],
 							},
-							"3":{
-								"label":"97",
-								"frequencies":[55, 40, 30, 12, 7, 43],
-								"weighted":	  [50, 45, 35,15, 7,44],
+							'3':{
+								'label':'97',
+								'frequencies':[55, 40, 30, 12, 7, 43],
+								'weighted':	  [50, 45, 35,15, 7,44],
 							}
 						},
-						"values":[-8,-2,-1,1,2,3,4],
-						"missings":[true, true,false,false,false,false],
-						"labels":["-8", "no response","Hauptschule","Realschule","Gymnasium","University"],
+						'values':[-8,-2,-1,1,2,3,4],
+						'missings':[true, true,false,false,false,false],
+						'labels':['-8', 'no response','Hauptschule','Realschule','Gymnasium','University'],
 					}
 				}
 			}
@@ -75,10 +75,10 @@
 			}
 			
 			if(options.weights == true){
-				dataType = "weighted"
+				dataType = 'weighted'
 			}
 			else{
-				dataType = "frequencies"
+				dataType = 'frequencies'
 			}
 			
 			
@@ -100,29 +100,29 @@
 			padding = 100;
 			barPadding = 1;
 			
-			var svg = d3.select("#chart")
-						.append("svg")
-						.attr("width", w)
-						.attr("height", h)
-						.attr("class", "chart");
+			var svg = d3.select('#chart')
+						.append('svg')
+						.attr('width', w)
+						.attr('height', h)
+						.attr('class', 'chart');
 										
 						
-			rects = svg.selectAll("rect")
+			rects = svg.selectAll('rect')
 					.data(data)
 					.enter()
-					.append("rect")
-					.attr("class", "rects");
+					.append('rect')
+					.attr('class', 'rects');
 			
-			text = svg.selectAll("text")
+			text = svg.selectAll('text')
 					.data(data)
 					.enter()
-					.append("text")
-					.attr("class", "text");
+					.append('text')
+					.attr('class', 'text');
 	
 
 		if(options.percent == true){
 			var sum =  d3.sum(data.map(function(d){return d[1] }));
-			format = d3.format("0.1%");
+			format = d3.format('0.1%');
 			text.text(function(d) {return format(d[1] / sum)}) 					
 		}
 		else{
@@ -132,7 +132,7 @@
 		var xScale = d3.scale.linear()
 						.domain([0, d3.max(data, function(d) {
 							return d3.max(d.filter(function(value) {
-							return typeof value === "number";
+							return typeof value === 'number';
 							}));
 						})])
 						.range([0, w - padding]);
@@ -148,34 +148,34 @@
 		// X-Achse
 		var xAxis = d3.svg.axis()
 						.scale(xScale)
-						.orient("bottom");
+						.orient('bottom');
 						
 		// Y-Achse
 		var yAxis = d3.svg.axis()
 						.scale(yScale)
-						.orient("left");				
+						.orient('left');				
 		
-		svg.append("g")
+		svg.append('g')
 			.call(xAxis)
-			.attr("class", "axis")
-			.attr("transform", "translate(" + padding + "," + (h-padding)+")");			
+			.attr('class', 'axis')
+			.attr('transform', 'translate(' + padding + ',' + (h-padding)+')');			
 			
-		svg.append("g")
+		svg.append('g')
 			.call(yAxis)
-			.attr("class", "axis")
-			.attr("transform", "translate(" + padding + ",0)");			
+			.attr('class', 'axis')
+			.attr('transform', 'translate(' + padding + ',0)');			
 		
 		
-		rects.attr("x", 0) 
-			 .attr("y", function(d) {return yScale(d[0])})
-			 .attr("width", function(d){return xScale(d[1])}) 
-			 .attr("height", ((h -padding) / data.length) - barPadding)
-			 .attr("transform", "translate(" + padding + ",0)");			
+		rects.attr('x', 0) 
+			 .attr('y', function(d) {return yScale(d[0])})
+			 .attr('width', function(d){return xScale(d[1])}) 
+			 .attr('height', ((h -padding) / data.length) - barPadding)
+			 .attr('transform', 'translate(' + padding + ',0)');			
 
 
 		barHeight = ((h -padding) / data.length) - barPadding;
-		text.attr("x", function(d) { return xScale(d[1])-3})
-			.attr("y", function(d) {return yScale(d[0]) + (barHeight/2)} )
-			.attr("transform", "translate(" + padding + ",0)");
+		text.attr('x', function(d) { return xScale(d[1])-3})
+			.attr('y', function(d) {return yScale(d[0]) + (barHeight/2)} )
+			.attr('transform', 'translate(' + padding + ',0)');
 
 }
--- chart.js
+++ chart.js
@@ -3,73 +3,73 @@ var hideMissings;
 var colors;
 var format; 
 
-function organizeData(options, menu_c_active){
+function organizeData(options, menu2_active){
 	
 				var rawData = { 
 		  
-				"study":"soep-test",
-				"dataset":"test1",
-				"variable":"edu",
-				"label":"Hoechster Bildungsabschluss",
-				"uni":{
-					"frequencies":[20, 12,123,321,214,100],
-					"weighted":	  [30, 5, 123, 400, 200, 90],
-					"values":	  [-8,-2,-1,1,2,3,4],
-					"missings":[true, true,false,false,false,false],
-					"labels":["-8", "no response","Hauptschule","Realschule","Gymnasium","University"],
+				'study':'soep-test',
+				'dataset':'test1',
+				'variable':'edu',
+				'label':'Hoechster Bildungsabschluss',
+				'uni':{
+					'frequencies':[20, 12,123,321,214,100],
+					'weighted':	  [30, 5, 123, 400, 200, 90],
+					'values':	  [-8,-2,-1,1,2,3,4],
+					'missings':[true, true,false,false,false,false],
+					'labels':['-8', 'no response','Hauptschule','Realschule','Gymnasium','University'],
 				},
-				"bi":{
-					"sex":{
-						"label":"Geschlecht",
-						"categories":{
-							"0":{
-								"label":"Mann",
-								"frequencies":[4, 1, 2, 3, 4, 5],
-								"weighted":	  [5, 13, 3, 7, 9, 6],
+				'bi':{
+					'sex':{
+						'label':'Geschlecht',
+						'categories':{
+							'0':{
+								'label':'Mann',
+								'frequencies':[4, 1, 2, 3, 4, 5],
+								'weighted':	  [5, 13, 3, 7, 9, 6],
 							},
-							"1":{
-								"label":"Frau",
-								"frequencies":[4, 2, 3, 6, 8, 10],
-								"weighted":	  [2, 4, 3, 7, 11, 13],
+							'1':{
+								'label':'Frau',
+								'frequencies':[4, 2, 3, 6, 8, 10],
+								'weighted':	  [2, 4, 3, 7, 11, 13],
 							}
 						},
-						"values":[-8,-2,-1,1,2,3,4],
-						"missings":[true, true,false,false,false,false],
-						"labels":["-8", "no response","Hauptschule","Realschule","Gymnasium","University"],
+						'values':[-8,-2,-1,1,2,3,4],
+						'missings':[true, true,false,false,false,false],
+						'labels':['-8', 'no response','Hauptschule','Realschule','Gymnasium','University'],
 					},
-					"wave":{
-						"label":"Welle",
-						"categories":{
-							"0":{
-								"label":"94",
-								"frequencies":[30, 60, 10, 4, 24, 35],
-								"weighted":	  [20, 90, 10, 3, 20, 34],
+					'wave':{
+						'label':'Welle',
+						'categories':{
+							'0':{
+								'label':'94',
+								'frequencies':[30, 60, 10, 4, 24, 35],
+								'weighted':	  [20, 90, 10, 3, 20, 34],
 							},
-							"1":{
-								"label":"95",
-								"frequencies":[34, 33, 2, 9, 8, 10],
-								"weighted":	  [30, 30, 4, 10, 7, 6],
+							'1':{
+								'label':'95',
+								'frequencies':[34, 33, 2, 9, 8, 10],
+								'weighted':	  [30, 30, 4, 10, 7, 6],
 							},
-							"2":{
-								"label":"96",
-								"frequencies":[45, 70, 30, 65, 83, 10],
-								"weighted":	  [45, 80, 20, 50, 60, 6],
+							'2':{
+								'label':'96',
+								'frequencies':[45, 70, 30, 65, 83, 10],
+								'weighted':	  [45, 80, 20, 50, 60, 6],
 							},
-							"3":{
-								"label":"97",
-								"frequencies":[55, 40, 30, 12, 7, 43],
-								"weighted":	  [50, 45, 35,15, 7,44],
+							'3':{
+								'label':'97',
+								'frequencies':[55, 40, 30, 12, 7, 43],
+								'weighted':	  [50, 45, 35,15, 7,44],
 							}
 						},
-						"values":[-8,-2,-1,1,2,3,4],
-						"missings":[true, true,false,false,false,false],
-						"labels":["-8", "no response","Hauptschule","Realschule","Gymnasium","University"],
+						'values':[-8,-2,-1,1,2,3,4],
+						'missings':[true, true,false,false,false,false],
+						'labels':['-8', 'no response','Hauptschule','Realschule','Gymnasium','University'],
 					}
 				}
 			}
 			
 	colors = d3.scale.category20();
-		colors.domain(rawData.bi[menu_c_active].labels)
+		colors.domain(rawData.bi[menu2_active].labels)
 		
 	
   
@@ -81,33 +81,33 @@ function organizeData(options, menu_c_active){
 	}
 	
 	if(options.percent == true){
-		offset = "expand";
-		format = d3.format("%");
+		offset = 'expand';
+		format = d3.format('%');
 	}
 	else{
-		offset = "";
-		format = d3.format("s");
+		offset = '';
+		format = d3.format('s');
 	}
 	if(options.weights == true){
-		dataType = "weighted"
+		dataType = 'weighted'
 	}
 	else{
-		dataType = "frequencies"
+		dataType = 'frequencies'
 	}
 		
 	var data  = [];
 	var indices = []
-	for(i = 0; i < rawData.bi[menu_c_active].missings.length; i++){
-		if(rawData.bi[menu_c_active].missings[i] == true){
+	for(i = 0; i < rawData.bi[menu2_active].missings.length; i++){
+		if(rawData.bi[menu2_active].missings[i] == true){
 			indices.unshift(i);
 		}
 	}
 				
-	for(i in rawData.bi[menu_c_active].categories){
-		id = rawData.bi[menu_c_active].categories[i].label;
+	for(i in rawData.bi[menu2_active].categories){
+		id = rawData.bi[menu2_active].categories[i].label;
 				
 		// Gewichtet oder nicht
-		freqs = rawData.bi[menu_c_active].categories[i][dataType];
+		freqs = rawData.bi[menu2_active].categories[i][dataType];
 			
 		// Missings oder nicht
 		if(hideMissings == true){
@@ -121,7 +121,7 @@ function organizeData(options, menu_c_active){
 				
 	}
 		
-	labels = rawData.bi[menu_c_active].labels;
+	labels = rawData.bi[menu2_active].labels;
 			if(hideMissings == true){
 				for(i in indices){
 					labels.splice(indices[i], 1);
@@ -140,11 +140,11 @@ function organizeData(options, menu_c_active){
 
 function drawChart(){
 	
-	d3.selectAll(".chart").remove();
+	d3.selectAll('.chart').remove();
 	
-	var tip = d3.select("body").append("tip")	
-						.attr("class", "tooltip")				
-						.style("opacity", 0);
+	var tip = d3.select('body').append('tip')	
+						.attr('class', 'tooltip')				
+						.style('opacity', 0);
 
 			var w =600;
 			var h = 300;
@@ -152,11 +152,11 @@ function drawChart(){
 			barPadding = 0.2;
 			barOutPadding = 0.1;
 								
-			var svg = d3.select("#chart")
-						.append("svg")
-						.attr("width", w)
-						.attr("height", h)
-						.attr("class", "chart");
+			var svg = d3.select('#chart')
+						.append('svg')
+						.attr('width', w)
+						.attr('height', h)
+						.attr('class', 'chart');
 			
 
 		var xScale = d3.scale.ordinal()
@@ -169,61 +169,63 @@ function drawChart(){
 
 		var xAxis = d3.svg.axis()
 						.scale(xScale)
-						.orient("bottom")
+						.orient('bottom')
 
 						
 	
 		var yAxis = d3.svg.axis()
 						.scale(yScale)
 						.tickFormat(format)
-						.orient("left");						
+						.orient('left');						
 		
-		svg.append("g")
+		svg.append('g')
 			.call(xAxis)
-			.attr("class", "axis")
-			.attr("transform", "translate(" + padding + "," + (h-padding)+")");			
+			.attr('class', 'axis')
+			.attr('transform', 'translate(' + padding + ',' + (h-padding)+')');			
 	
-		svg.append("g")
+		svg.append('g')
 			.call(yAxis)
-			.attr("class", "axis")
-			.attr("transform", "translate(" + padding + ",0)");		
+			.attr('class', 'axis')
+			.attr('transform', 'translate(' + padding + ',0)');		
 		
 
-         var layer = svg.selectAll("layer")
+         var layer = svg.selectAll('layer')
             .data(stacked)
             .enter()
-			.append("g")
-            .attr("class", "layer")
-            .style("fill", function(d){
+			.append('g')
+            .attr('class', 'layer')
+            .style('fill', function(d){
 				for(i in d){
 					return colors(d[i].label)
 				}
 			})
-			.attr("transform", "translate(" + padding + ",0)");			
+			.attr('transform', 'translate(' + padding + ',0)');			
 
            
-        var rect = layer.selectAll("rect")
+        var rect = layer.selectAll('rect')
             .data(function(d){return d})
             .enter()
-			.append("rect")
-            .attr("x", function(d) {return xScale(d.x)})
-            .attr("y", function(d) {return yScale(d.y + d.y0)})
-            .attr("height", function(d) {return yScale(d.y0) - yScale(d.y + d.y0)})
-            .attr("width", xScale.rangeBand())
-			.attr("class", "rect")
-			.on("mouseover", function(d) {		
+			.append('rect')
+            .attr('x', function(d) {return xScale(d.x)})
+            .attr('y', function(d) {return yScale(d.y + d.y0)})
+            .attr('height', function(d) {return yScale(d.y0) - yScale(d.y + d.y0)})
+            .attr('width', xScale.rangeBand())
+			.attr('class', 'rect')
+			.on('mouseover', function(d) {		
 				tip.transition()			
-					.style("opacity", .9);		
-				tip.html("<strong>" +d.label + "</strong>: " + d3.format(".2f")(d.y))	
-					.style("left", (d3.event.pageX) + "px")		
-					.style("top", (d3.event.pageY) + "px");	
+					.style('opacity', .9);		
+				tip.html('<strong>' +d.label + '</strong>: ' + d3.format('.2f')(d.y))	
+					.style('left', (d3.event.pageX) + 'px')		
+					.style('top', (d3.event.pageY) + 'px');	
             })					
-			.on("mouseout", function(d) {		
+			.on('mouseout', function(d) {		
 				tip.transition()			
-					.style("opacity", 0);	
+					.style('opacity', 0);	
 			});
 		
-}	
+}
+
+	
 
 
 
